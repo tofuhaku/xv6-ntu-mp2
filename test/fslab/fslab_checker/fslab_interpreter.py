a0fa2d573c5cc9119d4f28f3ccf01906872c4016
tofuhaku <tzy8179@gmail.com>
2026-03-31 14:18:44 +0800

    final

--- test/fslab/fslab_checker/fslab_interpreter.py
+++ test/fslab/fslab_checker/fslab_interpreter.py
@@ -1,11 +1,16 @@
 import json
 from typing import Any, Literal, Set, Union, List, Dict, Optional, Tuple
 
-from . import fslab_data_models as models
-from .fslab_data_models import KmemCache, Slab, Obj
-from .fslab_messages import SlabMsg, SlabCreateMsg, SlabAllocMsg, SlabFreeMsg, SlabPrintMsg
-from .fslab_parser import SlabMatcher
-from .fslab_utils import is_in_same_page
+# from . import fslab_data_models as models
+# from .fslab_data_models import KmemCache, Slab, Obj
+# from .fslab_messages import SlabMsg, SlabCreateMsg, SlabAllocMsg, SlabFreeMsg, SlabPrintMsg
+# from .fslab_parser import SlabMatcher
+# from .fslab_utils import is_in_same_page
+from . import fslab_data_models_v2 as models
+from .fslab_data_models_v2 import KmemCache, Slab, Obj
+from .fslab_messages_v2 import SlabMsg, SlabCreateMsg, SlabAllocMsg, SlabFreeMsg, SlabPrintMsg
+from .fslab_parser_v2 import SlabMatcher
+from .fslab_utils_v2 import is_in_same_page
 
 def run_msg(caches: Dict[str, KmemCache], slab_msg: SlabMsg, verbose = False):
     """Execute the given slab debug message and update caches accordingly."""
